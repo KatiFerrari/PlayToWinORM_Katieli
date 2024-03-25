89e8f068814ac4fd38c1f3cc1499767ada30e329
KatiFerrari <katielidasilvaferrari26@gmail.com>
2024-03-25 14:25:45 -0400

    atualizado-parte2-25-03

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const conn = require("./db/conn");
 
 const Ususario = require("/models/Usuario")
+const express = requere("express");
+const app= express = require();
+
+app.get("/Usuario/novo", (req, res) =>{
+    res.sendFile('${')
+}
+)
 conn
 .sync()
 .then(() => {
